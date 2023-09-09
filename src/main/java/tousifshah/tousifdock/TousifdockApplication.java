dc611513bb571817cf886793b1d236d3ee7fbdea
tousif <tousif@gmail.com>
2023-09-09 19:59:09 +0530

    commit

---
+++ src/main/java/tousifshah/tousifdock/TousifdockApplication.java
@@ -0,0 +1,13 @@
+package tousifshah.tousifdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TousifdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TousifdockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tousifshah/tousifdock/TousifdockApplicationTests.java
@@ -0,0 +1,13 @@
+package tousifshah.tousifdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TousifdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
